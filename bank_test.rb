214ba7f3fbd24e12549d1f4e7bfd9432586aa636
Jordan <jordanmanu@live.com.au>
2022-05-21 10:23:16 +0100

    refactored tests with context blocks

--- bank_test.rb
+++ bank_test.rb
@@ -1,26 +1,35 @@
+# frozen_string_literal: true
+
 class BankTest
   def initialize
     @current_balance = 0.00
     @statement = []
   end
+
   def balance
     @current_balance
   end
+
   def deposit(money)
-    deposit = @current_balance += money
-    transaction_details(Time.now.strftime("%d/%m/%Y"), money, "")
+    @current_balance += money
+    transaction_details(Time.now.strftime('%d/%m/%Y'), money, '')
   end
-  def transaction_details(date, credit, debit)
-    @statement.unshift({"date": date, "credit": credit.to_f, "debit": debit.to_f, "balance": @current_balance})
-  end
+
   def withdraw(money)
-    withdraw = @current_balance -= money
-    transaction_details(Time.now.strftime("%d/%m/%Y"),"", money)
+    @current_balance -= money
+    transaction_details(Time.now.strftime('%d/%m/%Y'), '', money)
   end
+
   def print_statement
-    print "date || credit || debit || balance"
+    print 'date || credit || debit || balance'
     @statement.each do |transaction|
-    print "#{transaction[:date]} || #{transaction[:credit]} || #{transaction[:debit]} || #{transaction[:balance]} \n"
+      print "#{transaction[:date]} || #{transaction[:credit]} || #{transaction[:debit]} || #{transaction[:balance]} \n"
     end
+  end
+
+  private
+
+  def transaction_details(date, credit, debit)
+    @statement.unshift({ "date": date, "credit": credit.to_f, "debit": debit.to_f, "balance": @current_balance })
   end
 end
--- spec/bank_test_spec.rb
+++ spec/bank_test_spec.rb
@@ -1,40 +1,57 @@
-require "./bank_test"
+# frozen_string_literal: true
 
-describe BankTest do 
+require './bank_test'
 
-  before(:each) do 
+describe BankTest do
+  before(:each) do
     @bank = BankTest.new
-    @date = Time.now.strftime("%d/%m/%Y")
+    @date = Time.now.strftime('%d/%m/%Y')
   end
 
-  it "initializes the account with an empty balance" do
-    expect(@bank.balance).to eq 0.00
+  context '#initialization' do
+    it 'initializes the account with an empty balance' do
+      expect(@bank.balance).to eq 0.00
+    end
+
+    it 'initializes the account with an empty statement' do
+      expect(@bank.print_statement).to eq []
+    end
   end
-  it "adds money to the account" do
-    @bank.deposit(1)
-    expect(@bank.balance).to eq 1
+
+  context '#deposit' do
+    it 'adds money to the account' do
+      @bank.deposit(1)
+      expect(@bank.balance).to eq 1
+    end
+
+    it 'accumulates the money added' do
+      @bank.deposit(3)
+      @bank.deposit(3)
+      expect(@bank.balance).to eq 6
+    end
   end
-  it "accumulates the money added" do
-    @bank.deposit(3)
-    @bank.deposit(3)
-    expect(@bank.balance).to eq 6
+  context '#withdraw' do
+    it 'withdraws money from the account' do
+      @bank.deposit(2)
+      @bank.withdraw(1)
+      expect(@bank.balance).to eq 1
+    end
   end
-  it "withdraws money from the account" do
-    @bank.deposit(2)
-    @bank.withdraw(1)
-    expect(@bank.balance).to eq 1
-  end
-  it "stores the date the transaction was made" do
-    @bank.deposit(2)
-    expect(@bank.print_statement).to eq [{:balance=>2.0, :credit=>2.0, :date=>"#{@date}", :debit=>0.0}]
-  end
-  it "prints the bank statement with date, credit and balance" do
-    @bank.deposit(5)
-    expect(@bank.print_statement).to eq [{:balance=>5.0, :credit=>5.0, :date=>"#{@date}", :debit=>0.0}]
-  end
-  it "prints the statement with most recent transaction first" do
-    @bank.deposit(15)
-    @bank.withdraw(10)
-    expect(@bank.print_statement[0]).to eq({:balance=>5.0, :credit=>0.0, :date=>@date, :debit=>10.0})
+  context '#print_statement' do
+    it 'stores the date the transaction was made' do
+      @bank.deposit(2)
+      expect(@bank.print_statement).to eq [{ balance: 2.0, credit: 2.0, date: @date.to_s, debit: 0.0 }]
+    end
+
+    it 'prints the bank statement with date, credit and balance' do
+      @bank.deposit(5)
+      expect(@bank.print_statement).to eq [{ balance: 5.0, credit: 5.0, date: @date.to_s, debit: 0.0 }]
+    end
+
+    it 'prints the statement with most recent transaction first' do
+      @bank.deposit(15)
+      @bank.withdraw(10)
+      expect(@bank.print_statement[0]).to eq({ balance: 5.0, credit: 0.0, date: @date, debit: 10.0 })
+    end
   end
 end
